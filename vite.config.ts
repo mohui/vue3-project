2b1c02ada810892eb25a29d49ce2356baa434ad6
王贺辉 <wanghehui@bjknrt.com>
2023-02-14 16:33:27 +0800

    别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 /**
  * 改端口
@@ -12,6 +13,12 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   server: {
     port: 3001
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname,"src"),
+      "com": path.resolve(__dirname,"src/components")
+    }
   },
   plugins: [vue()],
 })
