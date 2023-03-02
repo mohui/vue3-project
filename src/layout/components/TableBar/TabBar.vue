f7da614ff14c81507d36a04d8db51fac8a91b3ee
王贺辉 <wanghehui@bjknrt.com>
2023-03-02 14:21:47 +0800

    删除标签

--- src/layout/components/TableBar/TabBar.vue
+++ src/layout/components/TableBar/TabBar.vue
@@ -4,25 +4,28 @@
       type="card"
       editable
       @tab-click ="clickHandle"
+      @tab-remove="removeTab"
   >
     <el-tab-pane
         v-for="item in tabsList"
         :key="item.path"
         :label="item.title"
         :name="item.path"
+        closable
     >
       {{ item.content }}
     </el-tab-pane>
   </el-tabs>
 </template>
 <script lang="ts" setup>
-import {computed, ref, watch} from 'vue'
+import {computed, onMounted, ref, watch} from 'vue'
 import {useStore} from 'vuex'
-import {useRoute} from "vue-router";
+import {useRoute, useRouter} from "vue-router";
 import {Itab} from "@/store/type";
 
 const store = useStore()
 const route = useRoute()
+const router = useRouter()
 
 const tabsList = computed(()=>{
   return store.getters.getAddTab;
@@ -47,8 +50,56 @@ watch(()=> route.path, ()=>{
 })
 
 // 点击事件
-const clickHandle = (event: any) =>{
-  console.log(event.props.name)
-  // router.push({path: event.props.name})
+const clickHandle = (event: any) => {
+  router.push({path: event.props.name})
 }
+
+/**
+ * 移除tab
+ *
+ * 1. 如果是最后一个不让移除
+ * @param targetName
+ */
+const removeTab = (targetName: string) => {
+  console.log(tabsList.value.length)
+  // 1. 如果是最后一个不让移除
+  if(tabsList.value.length == 1) {
+    return alert("这已经是最后一个了")
+  }
+  // 2. 移除当前页
+  if (activeKey.value == targetName) {
+    tabsList.value.forEach((tab: Itab, index: number) => {
+      if (tab.path == targetName) {
+        const nextTab = tabsList.value[index + 1] || tabsList.value[index -1]
+        if (nextTab) {
+          activeKey.value = nextTab.path;
+        }
+      }
+    })
+  }
+  store.commit('closeCurrentTab', targetName)
+}
+
+// 刷新缓存数据
+const refresh = () => {
+  window.addEventListener('beforeunload', ()=>{
+    sessionStorage.setItem('TABS_ROUTES', JSON.stringify(tabsList.value));
+  })
+  // 拿到数据
+  let session = sessionStorage.getItem('TABS_ROUTES')
+  if (session) {
+    // 解析字符串
+    let tabItem = JSON.parse(session)
+    // 遍历
+    tabItem.forEach((tab: Itab) => {
+      store.commit('addTab', tab)
+    })
+  }
+}
+onMounted(() => {
+  // 初始化页面生成tab
+  addTab()
+  // 刷新保存数据
+  refresh()
+})
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -9,11 +9,17 @@ export const store = createStore<State>({
         tabsList: []
     },
     mutations: {
+        // 添加tab
         addTab(state: State, tab:Itab) {
             const isSome = state.tabsList.some((item)=> item.path == tab.path);
             if (!isSome) {
                 state.tabsList.push(tab)
             }
+        },
+        // 删除tab
+        closeCurrentTab(state: State, targetName: string) {
+            const index = state.tabsList.findIndex((item) => item.path == targetName);
+            state.tabsList.splice(index, 1);
         }
     },
     getters: {
