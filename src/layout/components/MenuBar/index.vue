1cea4e3718cf4dde41ddf8f90471c74205850713
王贺辉 <wanghehui@bjknrt.com>
2023-03-02 17:23:16 +0800

    显示出二级菜单

--- src/layout/components/MenuBar/index.vue
+++ src/layout/components/MenuBar/index.vue
@@ -4,38 +4,18 @@
       class="el-menu"
       :collapse="collapsed"
   >
-    <template v-for="item in routerMenu" :key="item.name">
-
-      <el-sub-menu v-if="item.children && item.children.length > 1" :index="item.name"  @click="toPath(item.name)">
-
-        <template #title>
-          <el-icon>
-            <component :is="item.meta?.icon" />
-          </el-icon>
-          <span>
-            {{ item.meta?.title }}多级
-          </span>
-        </template>
-      </el-sub-menu>
-
-
-      <el-menu-item :index="item.name" v-else  :route="{name: item.path}"  @click="toPath(item.name)">
-        <el-icon> <component :is="item.meta?.icon" /> </el-icon>
-        <template #title>{{ item.meta?.title }}一级</template>
-      </el-menu-item>
-    </template>
+    <menu-item :menus="routerMenu"></menu-item>
   </el-menu>
 </template>
 
 <script lang="ts" setup>
+import MenuItem from './menuItem.vue'
 import {computed} from 'vue'
 import {routeMenus} from '@/router'
 // 用点击事件跳转
 import {useRouter} from "vue-router";
 const router = useRouter()
-const toPath = (item: string) => {
-  router.push({name: item})
-}
+
 
 const routerMenu = computed(() => {
   return routeMenus.filter(it => {
--- src/layout/components/MenuBar/menuItem.vue
+++ src/layout/components/MenuBar/menuItem.vue
@@ -1,18 +1,33 @@
 <template>
   <template v-for="item in menus" :key="item.name">
 
-    <el-menu-item v-if="item.children && item.children.length > 1" :index="item.name">
-      <el-icon><icon-menu /></el-icon>
-      <template #title>{{ item.meta?.title }}aaa</template>
-    </el-menu-item>
+    <el-sub-menu v-if="item.children && item.children.length > 1" :index="item.name"  @click="toPath(item.name)">
 
-    <el-menu-item :index="item.name" v-else  :route="{name: item.path}">
-      <el-icon><icon-menu /></el-icon>
-      <template #title>{{ item.meta?.title }}ddd</template>
+      <template #title>
+        <el-icon>
+          <component :is="item.meta?.icon" />
+        </el-icon>
+        <span>
+            {{ item.meta?.title }}多级
+          </span>
+      </template>
+      <menu-item :menus="item.children"></menu-item>
+    </el-sub-menu>
+
+
+    <el-menu-item :index="item.name" v-else  :route="{name: item.path}"  @click="toPath(item.name)">
+      <el-icon> <component :is="item.meta?.icon" /> </el-icon>
+      <template #title>{{ item.meta?.title }}一级</template>
     </el-menu-item>
   </template>
 </template>
 
 <script setup lang="ts">
 defineProps(['menus'])
+// 用点击事件跳转
+import {useRouter} from "vue-router";
+const router = useRouter()
+const toPath = (item: string) => {
+  router.push({name: item})
+}
 </script>
